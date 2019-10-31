30d209043178f454fff83e61ac613724afc78b06
Leon <muserxu@gmail.com>
2019-10-31 16:17:06 -0400

    todo demo complete

--- todolist/src/app/app-routing.module.ts
+++ todolist/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './compoents/todos/todos.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:TodosComponent},
+  {path:'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

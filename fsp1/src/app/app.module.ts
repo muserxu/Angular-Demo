d113d7e04148a5b14f824ba6d99561fa11461968
Leon <muserxu@gmail.com>
2019-11-03 14:14:07 -0500

    add and delete

--- fsp1/src/app/app.module.ts
+++ fsp1/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { ItemsComponent } from './components/items/items.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
+
+import {ItemService} from './services/item.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'fsp1'),
+    AngularFirestoreModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- fsp1/src/environments/environment.ts
+++ fsp1/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAVj3DWc6sb-6HACbRXWYYWJZPCWRthCcY",
+    authDomain: "fs1prod-e36b8.firebaseapp.com",
+    databaseURL: "https://fs1prod-e36b8.firebaseio.com",
+    projectId: "fs1prod-e36b8",
+    storageBucket: "fs1prod-e36b8.appspot.com",
+    messagingSenderId: "676040669624",
+    appId: "1:676040669624:web:81264bb57e2ff58817b74d",
+    measurementId: "G-HJ8VX0WNWV"
+  }
 };
 
 /*
